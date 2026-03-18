9f5c031adcfad90185a511d76b5f5e895cbf634a
Ankit <ankitkumbhareanshul@gmail.com>
2026-03-18 17:30:14 +0530

    fix: update LinkedIn link

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 // Global Configuration Constants
 export const DASHBOARD_URL = import.meta.env.VITE_DASHBOARD_URL || "https://aura-wheat-six.vercel.app";
 export const GITHUB_URL = "https://github.com/Ankitcodes30/AURA.git";
-export const LINKEDIN_URL = "https://linkedin.com/in/ankitkumbhare";
+export const LINKEDIN_URL = "https://www.linkedin.com/in/ankit-kumbhare-024465366";
 export const GITHUB_PROFILE_URL = "https://github.com/Ankitcodes30";
